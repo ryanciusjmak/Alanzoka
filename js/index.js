32c01503e5893c2f6ffcdd512bb195f39de3ede3
Ryan Ciusjmak <ryanciusjmak@gmail.com>
2023-05-25 14:18:35 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var btn = document.querySelector(".no");
+var position
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    btn.addEventListener("touchstart", function () {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+    });
+
+    btn.addEventListener("touchend", function () {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+    });
+} else {
+    btn.addEventListener("mouseover", function () {
+        position ? (position = 0) : (position = 150);
+        btn.style.transform = `translate(${position}px,0px)`;
+        btn.style.transition = "all 0.3s ease";
+    });
+}
+
+function not() {
+    alert("Você nem ta maluca de não aceitar, volte lá e mete o dedo no sim! :)")
+}  
